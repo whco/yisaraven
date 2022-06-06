e5e9066431ebcd1fbec33c9f0d0e2ca85a79e116
whco <einsteized@gmail.com>
2022-06-07 00:37:38 +0900

    [Feat] 템플릿 엔진 Mustache Added; Index 및 등록 View 추가

--- src/test/java/com/yisaraven/web/HelloControllerTest.java
+++ src/test/java/com/yisaraven/web/HelloControllerTest.java
@@ -7,13 +7,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
+//@AutoConfigureMockMvc
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
