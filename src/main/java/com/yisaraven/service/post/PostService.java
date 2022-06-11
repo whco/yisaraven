15c76c74b2084e6b4045b6942829eee14aa4dc09
whco <einsteized@gmail.com>
2022-06-11 19:07:48 +0900

    [Feat] Post 조회 가능하게 변경

--- src/main/java/com/yisaraven/service/post/PostService.java
+++ src/main/java/com/yisaraven/service/post/PostService.java
@@ -2,13 +2,16 @@ package com.yisaraven.service.post;
 
 import com.yisaraven.web.domain.post.Post;
 import com.yisaraven.web.domain.post.PostRepository;
+import com.yisaraven.web.dto.PostListResponseDto;
 import com.yisaraven.web.dto.PostResponseDto;
 import com.yisaraven.web.dto.PostSaveRequestDto;
 import com.yisaraven.web.dto.PostUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +34,12 @@ public class PostService {
     public PostResponseDto findById(Long id) {
         Post entity = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostListResponseDto> findAllDesc() {
+        return postRepository.findAllDesc().stream()
+                .map(PostListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/yisaraven/web/IndexController.java
+++ src/main/java/com/yisaraven/web/IndexController.java
@@ -1,12 +1,18 @@
 package com.yisaraven.web;
 
+import com.yisaraven.service.post.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostService postService;
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("post", postService.findAllDesc());
         return "index";
     }
 
