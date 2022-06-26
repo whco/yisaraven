91affe649d97eb8f440d0a4e8dd6460fc381afce
whco <einsteized@gmail.com>
2022-06-27 00:44:01 +0900

    [Feat] Naver Login Added

--- src/main/java/com/yisaraven/config/auth/SecurityConfig.java
+++ src/main/java/com/yisaraven/config/auth/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
